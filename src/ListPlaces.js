e466d5c8611be96b94fd058d5b23d4d6f8b9d496
Alex Mejicanos <alexmejicanos@outlook.com>
2022-07-04 09:24:09 -0500

    Adding API and operations to frontend

--- src/ListPlaces.js
+++ src/ListPlaces.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listPlaces } from './graphql/queries';
 
 const ListPlaces = () => {
     const [places, setPlaces] = useState([]);
@@ -7,21 +9,29 @@ const ListPlaces = () => {
         fetchPlaces();
     }, []);
 
-    const fetchPlaces = () => {
-        setPlaces([
-            {
-                id: '1',
-                imgUrl: 'https://images.pexels.com/photos/3254729/pexels-photo-3254729.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                title: 'Madrid',
-                description: 'Very sunny and beautiful city',
-            },
-            {
-                id: '2',
-                imgUrl: 'https://images.pexels.com/photos/338515/pexels-photo-338515.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                title: 'Paris',
-                description: 'Rainy and nice place',
-            },
-        ]);
+    const fetchPlaces = async () => {
+        try {
+            const placesData = await API.graphql(graphqlOperation(listPlaces));
+            const placesItems = placesData.data.listPlaces.items;
+
+            setPlaces(placesItems);
+        } catch (error) {
+            console.log(`Error Fetching Places `, error);
+        }
+        // setPlaces([
+        //     {
+        //         id: '1',
+        //         imgUrl: 'https://images.pexels.com/photos/3254729/pexels-photo-3254729.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        //         title: 'Madrid',
+        //         description: 'Very sunny and beautiful city',
+        //     },
+        //     {
+        //         id: '2',
+        //         imgUrl: 'https://images.pexels.com/photos/338515/pexels-photo-338515.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        //         title: 'Paris',
+        //         description: 'Rainy and nice place',
+        //     },
+        // ]);
     };
 
     return (
